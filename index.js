330ef0b79c1381ccd0c8a98ae0c6cff521dbd912
Bstar098 <luceropintos@abc.gob.ar>
2023-10-30 17:35:23 -0300

    merge conflicting branches

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-<<<<<<< Updated upstream
-export const applyCustomCode = (externalCodeSetup) => {
-	// call custom code api here
-};
-=======
 import GroupFiltersCustom from "./components/GroupFiltersCustom";
-export const applyCustomCode = externalCodeSetup => {
-   externalCodeSetup.filterScreenApiHooks.setAfterFilterComponent(GroupFiltersCustom);
-}
->>>>>>> Stashed changes
+export const applyCustomCode = (externalCodeSetup) => {
+  externalCodeSetup.filterScreenApiHooks.setAfterFilterComponent(
+    GroupFiltersCustom
+  );
+};
